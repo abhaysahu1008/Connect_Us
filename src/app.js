e307160320579743bd227315c751e63a24a83e69
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-02-13 18:25:42 +0530

    express server created.

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// console.log("hello World");
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("My Dashboard");
+});
+app.get("/get", (req, res) => {
+  res.send("Hello World!!!!!!!!!");
+});
+app.get("/hey", (req, res) => {
+  res.send("Hellooooo");
+});
+
+app.listen(7777, () => {
+  console.log("Your server is listening on port 7777");
+});
