fdac2f63d7022a617051c79729d4d103e1649f6d
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-02-14 17:05:05 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -4,14 +4,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("My Dashboard");
-});
-app.get("/get", (req, res) => {
-  res.send("Hello World!!!!!!!!!");
-});
-app.get("/hey", (req, res) => {
-  res.send("Hellooooo");
+// app.get("/", (req, res) => {
+//   res.send("My Dashboard");
+// });
+// app.get("/get", (req, res) => {
+//   res.send("Hello World!!!!!!!!!");
+// });
+// app.get("/hey", (req, res) => {
+//   res.send("Hellooooo");
+// });
+
+app.get("/get/:user/:pass", (req, res) => {
+  console.log(req.params);
+
+  res.send({ name: "Abhay Sahu", city: "Jhansi" });
 });
 
 app.listen(7777, () => {
