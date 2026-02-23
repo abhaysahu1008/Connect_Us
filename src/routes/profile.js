90d3ef682238bf981dd3cc719bba91b014240720
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-02-23 23:47:36 +0530

    profile edit feature

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const User = require("../models/user");
+const { validateEditProfileData } = require("../utils/validation");
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
@@ -17,14 +18,22 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
 
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   try {
-    const user = req.user;
-    if (!user) {
-      throw new Error("User not found!");
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
     }
-    const editUser = await User.find;
-    // res.send(user);
-  } catch (error) {
-    res.status(400).send("Error : " + error.message);
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+
+    res.json({
+      message: `${loggedInUser.firstName}, your profile updated successfuly`,
+      data: loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
   }
 });
 
