2146fd61d997329783bbbb7f9bc4d6d1750d393c
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-02-24 23:11:10 +0530

    Sending connection request api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -19,19 +19,19 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   try {
     if (!validateEditProfileData(req)) {
-      throw new Error("Invalid Edit Request");
+      throw new Error("Invalid edit request!");
     }
 
     const loggedInUser = req.user;
 
     Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
-
     await loggedInUser.save();
 
     res.json({
       message: `${loggedInUser.firstName}, your profile updated successfuly`,
       data: loggedInUser,
     });
+    console.log(req.body);
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
