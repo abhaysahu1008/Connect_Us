ba6a0f3af1f9a113863466f015b8173c17118b11
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-03-01 19:51:46 +0530

    Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -53,11 +53,15 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
 userRouter.get("/feed", userAuth, async (req, res) => {
   const loggedInUser = req.user;
+
+  const page = parseInt(req.query.page) || 1;
+  let limit = parseInt(req.query.limit) || 10;
+  limit = limit > 50 ? 50 : limit;
+  const skip = (page - 1) * limit;
+
   const AllConnections = await ConnectionRequestModel.find({
     $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
   }).select("fromUserId toUserId");
-  // .populate("fromUserId", "firstName lastName")
-  // .populate("toUserId", "firstName lastName");
 
   const hideUsersFromFeed = new Set();
 
@@ -76,7 +80,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         _id: { $ne: loggedInUser._id },
       },
     ],
-  }).select(USER_SAFE_DATA);
+  })
+    .select(USER_SAFE_DATA)
+    .skip(skip)
+    .limit(limit);
 
   res.send(users);
 });
