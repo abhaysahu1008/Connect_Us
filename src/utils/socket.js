781d9c7c19d4d4ad070045f2a2c16096487bc8d6
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2026-04-18 19:12:10 +0530

    syntax error solved

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -17,64 +17,60 @@ const initializeSocket = (server) => {
         "http://localhost:5173",
 
         "https://devzoo.in",
-        "https://www.devzoo.in"
+        "https://www.devzoo.in",
       ],
       methods: ["GET", "POST"],
-      credentials: true
+      credentials: true,
     },
   });
 };
 
-  io.on("connection", (socket) => {
-    socket.on("joinChat", ({ firstName, userId, targetUser }) => {
-      const roomId = getCryptoRoomId(userId, targetUser);
-      socket.join(roomId);
+io.on("connection", (socket) => {
+  socket.on("joinChat", ({ firstName, userId, targetUser }) => {
+    const roomId = getCryptoRoomId(userId, targetUser);
+    socket.join(roomId);
 
-      console.log(firstName + " has joined the room :" + roomId);
-    });
-    socket.on(
-      "sendMessage",
-      async ({ firstName, userId, targetUser, text }) => {
-        const roomId = getCryptoRoomId(userId, targetUser);
-
-        try {
-          const AlreadyFriends = await ConnectionRequestModel.findOne({
-            $or: [
-              {
-                fromUserId: userId,
-                toUserId: targetUser,
-                status: "accepted",
-              },
-              { fromUserId: targetUser, toUserId: userId },
-            ],
-          });
-          if (!AlreadyFriends) return;
-
-          let chat = await Chats.findOne({
-            participants: { $all: [userId, targetUser] },
-          });
-
-          if (!chat) {
-            chat = new Chats({
-              participants: [userId, targetUser],
-              messages: [],
-            });
-          }
-          chat.messages.push({
-            senderId: userId,
-            text,
-          });
-
-          await chat.save();
-
-          io.to(roomId).emit("messageReceived", { firstName, text, userId });
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    );
-    socket.on("disconnect", () => {});
+    console.log(firstName + " has joined the room :" + roomId);
   });
-};
+  socket.on("sendMessage", async ({ firstName, userId, targetUser, text }) => {
+    const roomId = getCryptoRoomId(userId, targetUser);
+
+    try {
+      const AlreadyFriends = await ConnectionRequestModel.findOne({
+        $or: [
+          {
+            fromUserId: userId,
+            toUserId: targetUser,
+            status: "accepted",
+          },
+          { fromUserId: targetUser, toUserId: userId },
+        ],
+      });
+      if (!AlreadyFriends) return;
+
+      let chat = await Chats.findOne({
+        participants: { $all: [userId, targetUser] },
+      });
+
+      if (!chat) {
+        chat = new Chats({
+          participants: [userId, targetUser],
+          messages: [],
+        });
+      }
+      chat.messages.push({
+        senderId: userId,
+        text,
+      });
+
+      await chat.save();
+
+      io.to(roomId).emit("messageReceived", { firstName, text, userId });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  socket.on("disconnect", () => {});
+});
 
 module.exports = initializeSocket;
